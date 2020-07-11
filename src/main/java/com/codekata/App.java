78136f6f90f57c2f37f57fc621cb838233993a62
Sergey Meshchankin <smeshchankin@gmail.com>
2020-07-12 00:00:29 +0300

    [CK-000] Clear project

--- src/main/java/com/codekata/App.java
+++ src/main/java/com/codekata/App.java
@@ -1,13 +1,7 @@
 package com.codekata;
 
 public class App {
-    private static String[] array = new String[]{"арбуз", "апрель", "абрвал", "ноябрь", null};
-
-    public String getGreeting() {
-        return "Hello world.";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        System.out.println("Line Code Counter Application: lcc.jar");
     }
 }
