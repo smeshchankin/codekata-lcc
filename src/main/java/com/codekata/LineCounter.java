2489c0ce31fc93083720a14b7cd62bb43a9992fe
Sergey Meshchankin <smeshchankin@gmail.com>
2020-07-13 01:25:15 +0300

    [CK-004] Calculate lines : skip multiline comments

--- src/main/java/com/codekata/LineCounter.java
+++ src/main/java/com/codekata/LineCounter.java
@@ -1,10 +1,19 @@
 package com.codekata;
 
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Stream;
 
 public class LineCounter {
+
+  private static final int STANDARD = 0;
+  private static final int MULTILINE_COMMENT = 1;
+  private static final int SINGLE_LINE_COMMENT = 2;
+  // Java 13+ has multiline text blocks: *** multiline block ***
+  private static final int TEXT_BLOCK = 3;
+
   private final Stream<String> lines;
   private Long count = null;
+  private int state = STANDARD;
 
   public LineCounter(Stream<String> lines) {
     this.lines = lines;
@@ -15,13 +24,48 @@ public class LineCounter {
       count = this.lines == null ? 0 :
           this.lines
               .filter(s -> !s.isEmpty())
+              .filter(s -> !isMultilineComments(s))
               .filter(s -> !isSingleLineComment(s))
               .count();
     }
     return count;
   }
 
-  private boolean isSingleLineComment(String str) {
+  boolean isMultilineComments(String line) {
+    final char[] prev = new char[2];
+    AtomicBoolean hasValidCode = new AtomicBoolean(false);
+    // Notes: don't use .parallel() for escape incorrect characters ordering
+    line.chars().forEach(c -> {
+      char ch = (char) c;
+      if (!Character.isWhitespace(ch)) {
+        switch (ch) {
+          case '*':
+            if (prev[0] == '/') {
+              this.state = MULTILINE_COMMENT;
+            } else if (prev[0] == '*' && prev[1] == '*' && this.state != MULTILINE_COMMENT) {
+              this.state = this.state == TEXT_BLOCK ? STANDARD : TEXT_BLOCK;
+            }
+            break;
+          case '/':
+            if (prev[0] == '*' && this.state == MULTILINE_COMMENT) {
+              this.state = STANDARD;
+            }
+            break;
+          default:
+            if (this.state != MULTILINE_COMMENT) {
+              hasValidCode.set(true);
+            }
+        }
+      }
+
+      prev[1] = prev[0];
+      prev[0] = ch;
+    });
+
+    return !hasValidCode.get();
+  }
+
+  boolean isSingleLineComment(String str) {
     return str.trim().startsWith("//");
   }
 }
--- src/test/java/com/codekata/LineCounterTest.java
+++ src/test/java/com/codekata/LineCounterTest.java
@@ -1,6 +1,9 @@
 package com.codekata;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 import java.util.stream.Stream;
@@ -19,22 +22,66 @@ class LineCounterTest {
       "    }",
       "}",
       "");
+  private static Stream<String> STREAM_WITH_ONE_LINE_COMMENTS = Stream.of(
+      "// This is first comment",
+      "public class App {",
+      "    // This is main method",
+      "    public static void main() {",
+      "        // Print string",
+      "        System.out.println(\"Hello App\")",
+      "    } // End method main()",
+      "} // end class App");
+  private static Stream<String> STREAM_WITH_MULTI_LINE_COMMENTS = Stream.of(
+      "/*****",
+      " * This is a test program with 5 lines of code",
+      " *  \\/* no nesting allowed!",
+      " //*****//***/"
+  );
 
   @Test
-  void testCount_NullStream() {
+  public void testIsSingleLineCommentEmpty() {
+    LineCounter lineCounter = new LineCounter(null);
+    boolean isComment = lineCounter.isSingleLineComment("");
+    assertFalse(isComment, "Empty message isn't a comment");
+  }
+
+  @Test
+  public void testIsSingleLineCommentTrue() {
+    LineCounter lineCounter = new LineCounter(null);
+    boolean isComment = lineCounter.isSingleLineComment("   \t// My comment");
+    assertTrue(isComment, "Comment with whitespaces");
+  }
+
+  @Test
+  public void testIsMultiLineCommentTrue() {
+    final LineCounter lineCounter = new LineCounter(null);
+    STREAM_WITH_MULTI_LINE_COMMENTS.forEach(line -> {
+      boolean isComment = lineCounter.isMultilineComments(line);
+      assertTrue(isComment, "Multiline comments: [" + line + "]");
+    });
+  }
+
+  @Test
+  public void testCount_NullStream() {
     long count = new LineCounter(null).count();
-    Assertions.assertEquals(0, count, "Null Stream has 0 lines");
+    assertEquals(0, count, "Null Stream has 0 lines");
   }
 
   @Test
-  void testCount_EmptyStream() {
+  public void testCount_EmptyStream() {
     long count = new LineCounter(EMPTY_STREAM).count();
-    Assertions.assertEquals(0, count, "Empty file has 0 lines");
+    assertEquals(0, count, "Empty file has 0 lines");
   }
 
   @Test
-  void testCount_StreamWithEmptyLines() {
+  public void testCount_StreamWithEmptyLines() {
     long count = new LineCounter(STREAM_WITH_EMPTY_LINES).count();
-    Assertions.assertEquals(6, count, "Stream should skip empty lines");
+    assertEquals(6, count, "Stream should skip empty lines");
+  }
+
+  @Test
+  public void testFilter_StreamWithOneLineComments() {
+    long count = new LineCounter(STREAM_WITH_ONE_LINE_COMMENTS).count();
+    assertEquals(count, 5, "One line comments should be skipped");
   }
 }
