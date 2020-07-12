cfeedbe7285fe6c2f57ce7a8b53b543bb463d241
Sergey Meshchankin <smeshchankin@gmail.com>
2020-07-12 22:38:23 +0300

    [CK-004] Calculate lines : skip single line comments

--- src/main/java/com/codekata/LineCounter.java
+++ src/main/java/com/codekata/LineCounter.java
@@ -4,13 +4,24 @@ import java.util.stream.Stream;
 
 public class LineCounter {
     private final Stream<String> lines;
+    private Long count = null;
 
     public LineCounter(Stream<String> lines) {
         this.lines = lines;
     }
 
-    public long count() {
-        return this.lines == null ? 0 :
-            this.lines.filter(s -> !s.isEmpty()).count();
+    synchronized public long count() {
+        if (count == null) {
+            count = this.lines == null ? 0 :
+                this.lines
+                    .filter(s -> !s.isEmpty())
+                    .filter(s -> !isSingleLineComment(s))
+                    .count();
+        }
+        return count;
+    }
+
+    private boolean isSingleLineComment(String str) {
+        return str.trim().startsWith("//");
     }
 }
